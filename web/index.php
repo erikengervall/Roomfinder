7583225c70b6d9a1d53892f772a92e7069fb3069
Erik Engervall <erik.engervall@gmail.com>
2019-12-27 16:57:46 +0100

    add gtm

--- web/index.php
+++ web/index.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html ng-app="broom" lang="en">
   <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-57286064-4"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-57286064-4');
+  </script>
+  
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>
